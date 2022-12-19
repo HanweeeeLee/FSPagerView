b818e9e86f65fc13a1dee9ee7d7042a4b1af48bd
LEE HANWE <mobiledev@kakao.com>
2022-12-19 15:31:58 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,12 +4,22 @@ import PackageDescription
 let package = Package(
     name: "FSPagerView",
     platforms: [
-        .iOS(.v9)
+        .iOS(.v13)
     ],
     products: [
         .library(name: "FSPagerView", targets: ["FSPagerView"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "6.5.0"),
+    ]
     targets: [
-        .target(name: "FSPagerView", path: "Sources", exclude: ["FSPagerViewObjcCompat.h", "FSPagerViewObjcCompat.m"]),
+        .target(name: "FSPagerView", 
+                      dependencies: [
+                        .product(name: "RxSwift", package: "RxSwift"),
+                        .product(name: "RxCocoa", package: "RxSwift")
+                        ],
+                path: "Sources", 
+                exclude: ["FSPagerViewObjcCompat.h", "FSPagerViewObjcCompat.m"]
+                ),
     ]
 )
