ab16d743fab6137b6710352122d32e4ecd248bc1
LEE HANWE <mobiledev@kakao.com>
2022-12-19 15:32:55 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "6.5.0"),
-    ]
+    ],
     targets: [
         .target(name: "FSPagerView", 
                       dependencies: [
