457248f8d06cc146756a94a30b2b2a4dac884cba
LEE HANWE <mobiledev@kakao.com>
2023-02-09 18:03:52 +0900

    Update FSPagerViewRxProxy.swift

--- Sources/FSPagerViewRxProxy.swift
+++ Sources/FSPagerViewRxProxy.swift
@@ -1,84 +1,77 @@
 //
-//  RxProxy.swift
-//  
+//  RxFSPagerView.swift
+//  RxPagerView
 //
-//  Created by Hanwe LEE on 2022/12/19.
+//  Created by yangkejun on 2021/12/3.
 //
 
 import RxSwift
 import RxCocoa
-import Foundation
 
-class FSPagerViewDelegateProxy: DelegateProxy<FSPagerView, FSPagerViewDelegate>, DelegateProxyType, FSPagerViewDelegate {
-  
-  static func registerKnownImplementations() {
-    self.register { (viewController) -> FSPagerViewDelegateProxy in
-      FSPagerViewDelegateProxy(parentObject: viewController, delegateProxy: self)
-    }
-  }
-  
-  static func currentDelegate(for object: FSPagerView) -> FSPagerViewDelegate? {
-    return object.delegate
-  }
-  
-  static func setCurrentDelegate(_ delegate: FSPagerViewDelegate?, to object: FSPagerView) {
-    object.delegate = delegate
-  }
-  
-}
-
-extension Reactive where Base == FSPagerView {
-  
-  var delegate: DelegateProxy<FSPagerView, FSPagerViewDelegate> {
-    return FSPagerViewDelegateProxy.proxy(for: self.base)
-  }
-  
-  public var didSelectedItem: Observable<Int> {
-    return delegate.methodInvoked(#selector(FSPagerViewDelegate.pagerView(_:didSelectItemAt:)))
-      .map { param in
-        return param[1] as? Int ?? 0
-      }
-  }
-  
-  public typealias ConfigureCell<S: Sequence, Cell> = (Int, S.Iterator.Element, Cell) -> Void
-  
-  public func items<S: Sequence, Cell: FSPagerViewCell, O: ObservableType>(
-    cellIdentifier: String,
-    cellType: Cell.Type = Cell.self
-  ) -> (_ source: O) -> (_ configureCell: @escaping ConfigureCell<S, Cell>) -> Disposable
-  where O.Element == S {
-    base.collectionView.dataSource = nil
-    return { source in
-      let source = source.observe(on: ConcurrentDispatchQueueScheduler(queue: .global()))
-        .map { sequence -> [S.Element] in
-          let items: [S.Element] = {
-            var items = Array(sequence)
-            if items.count > 0 {
-              let lastItem = items.last!
-              let firstItme = items.first!
-              items.insert(lastItem, at: 0)
-              items.append(firstItme)
+public extension Reactive where Base: FSPagerView {
+    
+    /**
+     Binds sequences of elements to collection view items.
+     
+     - parameter cellIdentifier: Identifier used to dequeue cells.
+     - parameter source: Observable sequence of items.
+     - parameter configureCell: Transform between sequence elements and view cells.
+     - returns: Disposable object that can be used to unbind.
+     
+     Example
+     
+     let items = Observable.just(["image1", "image2", "image3"])
+     items.bind(to: pagerView.rx.items(cellIdentifier: "FSPagerViewCell")) { (row, element, cell) in
+     cell.imageView?.image = UIImage(named: element)
+     }.disposed(by: disposeBag)
+     
+     */
+    func items<Sequence: Swift.Sequence, Cell: FSPagerViewCell, Source: ObservableType>
+    (cellIdentifier: String? = String(describing: FSPagerViewCell.self))
+    -> (_ source: Source)
+    -> (_ configureCell: @escaping (Int, Sequence.Element, Cell) -> Void)
+    -> Disposable where Source.Element == Sequence {
+        base.collectionView.dataSource = nil
+        return { source in
+            let type: ImmediateSchedulerType = ConcurrentDispatchQueueScheduler(queue: .global())
+            let source = source.observe(on: type).map { sequence -> [Sequence.Element] in
+                let datas = Array(sequence)
+                guard datas.isEmpty == false else { return [] }
+                base.numberOfItems = Int(Int16.max)
+                base.numberOfSections = datas.count
+                let loop = datas.count > 1 || base.removesInfiniteLoopForSingleItem == false
+                let count = base.isInfinite && loop ? base.numberOfItems / datas.count : 1
+                var array: [Sequence.Element] = []
+                for _ in 0..<count { array += datas }
+                return array
             }
-            return items
-          }()
-          if base.isInfinite {
-            if items.count == 0 {
-              base.numberOfItems = 0
-            } else if items.count == 1 {
-              base.numberOfItems = 1
-            } else {
-              base.numberOfItems = Int(items.count - 2)
-            }
-          } else {
-            base.numberOfItems = items.count
-          }
-          return items
+            return base.collectionView.rx.items(cellIdentifier: cellIdentifier!, cellType: Cell.self)(source)
         }
-      return self.base.collectionView.rx.items(
-        cellIdentifier: cellIdentifier,
-        cellType: cellType
-      )(source)
     }
-  }
-  
 }
+
+public extension Reactive where Base: FSPagerView {
+    
+    /// Reactive wrapper for `delegate` message `FSPagerView(_:didSelectItemAtIndex:)`.
+    var didSelectItemAtIndex: ControlEvent<Int> {
+        let source = base.collectionView.rx.itemSelected.flatMap { IndexPath in
+            return Observable.just(IndexPath.row % base.numberOfSections)
+        }
+        return ControlEvent(events: source)
+    }
+    
+    /// Reactive wrapper for `delegate` message `FSPagerView(pagerViewDidScroll:)`.
+    var pagerViewDidScroll: ControlEvent<Int> {
+        let source = base.collectionView.rx.didScroll.flatMap { () -> Observable<Int> in
+            if base.numberOfItems > 0 {
+                let currentIndex = lround(Double(base.scrollOffset)) % base.numberOfItems
+                if (currentIndex != base.currentIndex) {
+                    return Observable.just(currentIndex % base.numberOfSections)
+                }
+            }
+            return Observable.never()
+        }
+        return ControlEvent(events: source)
+    }
+}
+
